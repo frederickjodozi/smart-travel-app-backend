62a17ea0985ae41134119a333423f6aa427c9bab
Fred Jodozi <fredfred776@gmail.com>
2023-01-17 20:11:32 -0600

    connect to mongodb and use routes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const routes = require('./routes/index');
 const { PORT = 3000, HOST = 'localhost' } = process.env;
 const app = express();
 
-// app.use(routes);
+mongoose.connect('mongodb://localhost:27017/smart-travel-app');
+
+app.use(routes);
 
 app.listen(PORT, HOST, () => console.log(`Listening on ${HOST}:${PORT}!`));
