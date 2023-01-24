05c02c7a203cf342cc747d48a0cdd9b94eca926f
Fred Jodozi <fredfred776@gmail.com>
2023-01-23 22:34:14 -0600

    add :id to mylocations delete route

--- routes/locations.js
+++ routes/locations.js
@@ -1,14 +1,9 @@
 const router = require('express').Router();
 
-const {
-  getLocations,
-  createLocation,
-  deleteLocation
-} = require('../controllers/locations');
+const { getLocations, createLocation, deleteLocation } = require('../controllers/locations');
 
-router.route('/mylocations')
-  .get(getLocations)
-  .post(createLocation)
-  .delete(deleteLocation);
+router.route('/mylocations').get(getLocations).post(createLocation);
+
+router.delete('/mylocations/:id', deleteLocation);
 
 module.exports = router;
