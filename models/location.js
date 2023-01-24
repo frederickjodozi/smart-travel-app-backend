8346a182e4ffea333f5beae687a6ed5cfcf8cc46
Fred Jodozi <fredfred776@gmail.com>
2023-01-23 22:37:26 -0600

    add timestamps and disable text and __v field rendering

--- models/location.js
+++ models/location.js
@@ -1,31 +1,43 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const locationSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    minLength: 3,
-    maxLength: 30,
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-    validator: {
-      validator(imageUrl) {
-        return validator.isURL(imageUrl);
-      },
-      message: 'The location image must be a valid URL'
+const locationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      minLength: 3,
+      maxLength: 30,
+      required: true
     },
-    required: true
+    text: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      validator: {
+        validator(imageUrl) {
+          return validator.isURL(imageUrl);
+        },
+        message: 'The location image must be a valid URL'
+      },
+      required: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true
+    }
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+  }
+);
+
+locationSchema.set('toJSON', {
+  transform: function (doc, ret, options) {
+    delete ret.__v;
+    delete ret.text;
   }
 });
 
