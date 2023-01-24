577d6fed3ebaf97375a7db25676a07b9798312c1
Fred Jodozi <fredfred776@gmail.com>
2023-01-23 22:35:01 -0600

    create getLocations, createLocation and deleteLocation controllers

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,14 +1,55 @@
-const locations = require('../models/location');
+const asyncHandler = require('express-async-handler');
+const Location = require('../models/location');
 
-const getUserLocations = (req, res, next) => {
-  const { user } = req;
+const getLocations = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
 
-  locations
-    .find({ owner: user })
-    .then((locations) => res.send(locations))
-    .catch((err) => {
-      next(err);
-    });
-};
+  const userLocations = await Location.find({ owner: userId });
 
-module.exports = getUserLocations;
+  if (!userLocations) {
+    throw new Error(`Couldn't find any saved locations for this user`);
+  }
+
+  res.status(200).json(userLocations);
+});
+
+const createLocation = asyncHandler(async (req, res) => {
+  const { title, text, image } = req.body;
+  const { userId } = req.body;
+
+  if (!title || !text || !image) {
+    throw new Error('The location needs a title, a text and an image');
+  }
+
+  const createdLocation = await Location.create({ title, text, image, owner: userId });
+
+  if (!createdLocation) {
+    throw new Error(`Couldn't create location`);
+  }
+
+  const returnedLocation = await Location.findById(createdLocation._id, '-updated_at');
+  res.status(200).json(returnedLocation);
+});
+
+const deleteLocation = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+  const { id } = req.params;
+
+  const locationToDelete = await Location.findById(id);
+
+  if (!locationToDelete) {
+    throw new Error(`Couldn't find location to delete`);
+  } else if (!locationToDelete.owner.equals(userId)) {
+    throw new Error(`Cannot delete other user's locations`);
+  }
+
+  const deletedLocation = await Location.findByIdAndDelete(locationToDelete._id);
+
+  if (!deletedLocation) {
+    throw new Error(`Couldn't delete location`);
+  }
+
+  res.status(204).json('');
+});
+
+module.exports = { getLocations, createLocation, deleteLocation };
