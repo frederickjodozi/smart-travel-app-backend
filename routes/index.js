445fbd2e6091b6eddd1c3a77869e82b3d8d0156d
Fred Jodozi <fredfred776@gmail.com>
2023-01-23 19:05:34 -0600

    refactor route for resources not found with asyncHandler

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
 
 const userRouter = require('./users');
 const locationRouter = require('./locations');
@@ -6,8 +7,10 @@ const locationRouter = require('./locations');
 router.use('/users', userRouter);
 router.use('/locations', locationRouter);
 
-router.use((req, res, next) => {
-  next(new Error('Requested resource not found'));
-});
+router.use(
+  asyncHandler(async (req, res) => {
+    throw new Error('Requested resource not found');
+  })
+);
 
 module.exports = router;
