8655c0ed93476fc35476c6143d54b1288c0b9b7e
Fred Jodozi <fredfred776@gmail.com>
2023-01-26 22:59:23 -0600

    add helmet, rate-limit and cors security features

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const limiter = require('./middleware/limiter');
+const cors = require('cors');
 const { registerUser, userLogin } = require('./controllers/users');
 const {
   validateUserRegistry,
@@ -14,16 +17,29 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/smart-travel-app');
 
+// SECURITY //
+app.use(helmet());
+
+app.use(limiter);
+
+app.use(cors());
+
+app.options('*', cors());
+
+// PARSING //
 app.use(express.json());
 
+// ROUTES //
 app.post('/users/register', validateUserRegistry, registerUser);
 
 app.post('/users/login', validateUserLogin, userLogin);
 
 app.use(routes);
 
+//ERROR HANDLING //
 app.use(errors());
 
 app.use(errorHandler);
 
+// APP.LISTEN //
 app.listen(PORT, HOST, () => console.log(`Listening on ${HOST}:${PORT}!`));
